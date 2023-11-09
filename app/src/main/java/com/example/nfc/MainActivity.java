787a692886e93c3b471729a0e85687cc5c117b0e
Josh Luzier <joshua.luzier.19@cnu.edu>
2023-11-09 09:54:03 -0500

    Adding some of the required NDEF components. Need to add more still.

--- app/src/main/java/com/example/nfc/MainActivity.java
+++ app/src/main/java/com/example/nfc/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.nfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
     private static final List<String> PROTOCOLS = Arrays.asList("http://", "https://");
     private static final int SELECTED_ITEM = 0;
     private String selected_url;
+    private Uri created_uri;
     private String selected_protocol;
     Button button;
     TextView textView;
     Spinner spinner;
+    Tag my_tag;
+    Context context;
 
     // write to NFC tag
 
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.text);
         spinner = findViewById(R.id.spinner);
         setSpinner();
+        context = this;
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
@@ -56,8 +64,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selected_url = textView.getText().toString();
                 Toast.makeText(MainActivity.this, selected_protocol + selected_url, Toast.LENGTH_SHORT).show();
+                created_uri = Uri.parse("selected_url");
+                NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(context);
+                my_tag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                writeTag(created_uri, my_tag);
             }
         });
+    }
+
+    private void writeTag(Uri createdUri, Tag tag) {
+
     }
 
     @Override
